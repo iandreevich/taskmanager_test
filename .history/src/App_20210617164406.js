439c9e5bf3a2db85a4a1c3dc3cf6620f10f767d0
iandreevich <iadnreevich228@gmail.com>
2021-06-24 09:42:21 +0300

    remove history

--- .history/src/App_20210617164406.js
+++
@@ -1,7 +0,0 @@
-import { DragDropContext, Droppable, Dreggable } from "react-beautiful-dnd";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
--- .history/src/App_20210617174156.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <Board />
-    </div>
-  );
-}
-
-export default App;
